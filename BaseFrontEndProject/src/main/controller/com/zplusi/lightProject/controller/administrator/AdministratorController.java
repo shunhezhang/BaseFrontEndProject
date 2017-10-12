37add9748c54176f7ca54b5491aa4bf4eb58f506
xky <xky@stun.server.1>
2017-10-12 21:07:39 +0800

    Test commit

--- BaseFrontEndProject/src/main/controller/com/zplusi/lightProject/controller/administrator/AdministratorController.java
+++ BaseFrontEndProject/src/main/controller/com/zplusi/lightProject/controller/administrator/AdministratorController.java
@@ -13,7 +13,7 @@ import com.zplusi.lightProject.service.administrator.AdministratorService;
 
 /**
  * =======================<br/>
- * 管理员管理控制类，处理管理员管理的前后端交互
+ * 管理员管理控制类，处理管理员管理的前后端交互：test
  * Date：2016-12-25 22:31:10
  * Author：shunhezhang
  * =======================<br/>
